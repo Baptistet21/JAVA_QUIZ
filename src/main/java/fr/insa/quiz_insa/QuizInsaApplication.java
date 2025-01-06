784d15ad011631a30c425acbb9ee8c701746882f
Baptistet21 <102140472+Baptistet21@users.noreply.github.com>
2025-01-06 10:07:13 +0100

    push

---
+++ src/main/java/fr/insa/quiz_insa/QuizInsaApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.quiz_insa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizInsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizInsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/insa/quiz_insa/QuizInsaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.quiz_insa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizInsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
