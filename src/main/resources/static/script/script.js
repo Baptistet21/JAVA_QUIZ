78d0feb1e402fa4d18b1c9408f2f6dbad947aeff
Baptistet21 <102140472+Baptistet21@users.noreply.github.com>
2025-01-09 16:46:43 +0100

    connexion et inscption securite + erreur

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,10 +1,9 @@
-function togglePasswordVisibility() {
-    var passwordFields = document.querySelectorAll('#password, #confirm_password');
-    passwordFields.forEach(function(field) {
-        if (field.type === 'password') {
-            field.type = 'text';
-        } else {
-            field.type = 'password';
-        }
-    });
+function togglePasswordVisibility(button) {
+    const input = button.previousElementSibling;
+    if (input.type === "password") {
+    input.type = "text";
+} else {
+    input.type = "password";
 }
+}
+
