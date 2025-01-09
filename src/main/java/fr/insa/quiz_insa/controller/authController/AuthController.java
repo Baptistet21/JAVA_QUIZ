64bdfec12a0c94856f07bac037ec4fa1b2941c87
Baptistet21 <102140472+Baptistet21@users.noreply.github.com>
2025-01-09 14:22:28 +0100

    push page sans fonction

--- src/main/java/fr/insa/quiz_insa/controller/authController/AuthController.java
+++ src/main/java/fr/insa/quiz_insa/controller/authController/AuthController.java
@@ -1,14 +1,28 @@
 package fr.insa.quiz_insa.controller.authController;
 
 import fr.insa.quiz_insa.model.DTO.UserDTO;
+import fr.insa.quiz_insa.model.Utilisateur;
 import fr.insa.quiz_insa.model.security.LoginRequest;
+import fr.insa.quiz_insa.repository.UtilisateurRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @Controller
 public class AuthController {
+    @Autowired
+//    private UserService userService;
+    private UtilisateurRepository utilisateurRepository;
+
 
     /* renvoie la page html de connexion*/
     @GetMapping("/connexion")
@@ -23,6 +37,39 @@ public class AuthController {
         model.addAttribute("utilisateur", new UserDTO());
         return "view/auth/inscription" ;
     }
+//
+//    @PostMapping("/registration")
+//    public String registration(
+//            @ModelAttribute("utilisateur") @Valid UserDTO userDTO, BindingResult bindingResult,
+//            Model model
+//    ) throws IllegalArgumentException {
+//        Utilisateur existingEmail = utilisateurRepository.findByEmail(userDTO.getEmail());
+//
+//
+//        if (bindingResult.hasErrors()) {
+//            model.addAttribute("errors", bindingResult.getAllErrors());
+//            return "view/auth/inscription" ;
+//        }
+//
+//        /* verification email */
+//        if (existingEmail != null) {
+//            userDTO.setEmailError("Un compte existe déjà avec cet email.");
+//            return "view/auth/inscription" ;
+//        }
+//        /* verification password */
+//        if (!(userDTO.getPassword().equals(userDTO.getConfirmPassword()))) {
+//            userDTO.setPasswordError("Les mots de passe ne correspondent pas.");
+//            return "view/auth/inscription" ;
+//        }
+//
+//        /* verif password condition */
+//
+//        userService.saveUser(userDTO);
+//        /* si inscription est correct renvoie vers page connexion */
+//        model.addAttribute("req", new LoginRequest());
+//        return CONNEXION_VIEW;
+//    }
+
 
 
 }
